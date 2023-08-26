9c917547993bf08388d94457f74f0a7539e40fd7
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-08-26 21:06:10 +0700

    Update konfigurasi.sh

--- konfigurasi.sh
+++ konfigurasi.sh
@@ -1,13 +1,16 @@
-echo "SETING IP ADDRESS STATIK"
-echo "------------------------"
-echo ""
+echo "------------------------------"
+echo "   SETING IP ADDRESS STATIK   "
+echo "------------------------------"
+echo "       Dibuat oleh Rofi       "
+echo " https://github.com/R0P1/SIAS "
+echo "------------------------------"
 
-read -p "Masukkan ip address: " address
-read -p "Masukkan netmask: " netmask
-read -p "Masukkan ip network: " network
-read -p "Masukkan ip broadcast: " broadcast
-read -p "Masukkan gateway: " gateway
-read -p "Masukkan dns-nameservers: " dns_nameservers
+read -p "Masukkan alamat IP (contoh: 192.168.1.14): " address
+read -p "Masukkan netmask (contoh: 255.255.255.0): " netmask
+read -p "Masukkan jaringan IP (contoh: 192.168.1.0): " network
+read -p "Masukkan alamat siaran IP (contoh: 192.168.1.255): " broadcast
+read -p "Masukkan alamat gateway IP (contoh: 192.168.1.1): " gateway
+read -p "Masukkan dns-nameservers (contoh: 8.8.8.8): " dns_nameservers
 
 file_konfigurasi="/etc/network/interfaces"
 
@@ -20,3 +23,10 @@ echo "	network ${network}" >> ${file_konfigurasi}
 echo "	broadcast ${broadcast}" >> ${file_konfigurasi}
 echo "	gateway ${gateway}" >> ${file_konfigurasi}
 echo "	dns-nameservers ${dns_nameservers}" >> ${file_konfigurasi}
+
+echo "[*] Sedang melakukan konfigurasi..."
+
+sleep 14
+
+echo "[+] Selesai."
+echo "[*] Silahkan restart sistem operasi Anda dengan menjalankan perintah 'reboot'."
