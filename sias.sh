cc196b6580fbc28ece1c606cd10babee0a29a8cf
R0P1 <139617581+R0P1@users.noreply.github.com>
2023-08-27 03:59:06 +0700

    Update sias.sh

--- sias.sh
+++ sias.sh
@@ -9,37 +9,32 @@ echo "| E-mail  | pysoft.biz@gmail.com             |                  |"
 echo "+============================================+"
 echo ""
 
-read -p "Masukkan alamat IP (contoh: 192.168.1.14): " address
-read -p "Masukkan netmask (contoh: 255.255.255.0): " netmask
-read -p "Masukkan jaringan IP (contoh: 192.168.1.0): " network
-read -p "Masukkan alamat siaran IP (contoh: 192.168.1.255): " broadcast
-read -p "Masukkan alamat gateway IP (contoh: 192.168.1.1): " gateway
-read -p "Masukkan dns-nameservers (contoh: 8.8.8.8): " dns_nameservers
+read -p "Masukkan Ethernet (contoh: eth0): " e
+read -p "Masukkan alamat IP (contoh: 192.168.1.14): " a
+read -p "Masukkan prefix alamat IP (contoh: 24): " p
+read -p "Masukkan alamat gateway IP (contoh: 192.168.1.1): " g
+read -p "Masukkan dns-nameservers (contoh: 8.8.8.8): " d
 
 # file konfigurasi 
-file_interfaces="/etc/network/interfaces"
-file_resolv="/etc/resolv.conf"
+f=/etc/netplan/01-netcfg.yaml
 
-# konfigurasi pada file interfaces
-echo "auto eth0" >> ${file_interfaces}
-echo "iface eth0 inet static" >> ${file_interfaces}
-echo "	address ${address}" >> ${file_interfaces}
-echo "	netmask ${netmask}" >> ${file_interfaces}
-echo "	network ${network}" >> ${file_interfaces}
-echo "	broadcast ${broadcast}" >> ${file_interfaces}
-echo "	gateway ${gateway}" >> ${file_interfaces}
-echo "	dns-nameservers ${dns_nameservers}" >> ${file_interfaces}
-
-# konfigurasi pada file resolv.conf
-echo "nameserver ${dns_nameservers}" >> ${file_resolv}
-
-# restart layanan jaringan pada sistem linux 
-/etc/init.d/networking restart
-
-echo ""
 echo "[*] Sedang melakukan konfigurasi..."
-
 sleep 14
 
+echo "network:" >> $f
+echo "        version: 2" >> $f
+echo "        renderer: networkd" >> $f
+echo "        ethernets:" >> $f
+echo "                $e:" >> $f
+echo "                dhcp4: no" >> $f
+echo "                addresses:" >> $f
+echo "                        - $a/$p" >> $f
+echo "                gateway4: $g" >> $f
+echo "                nameservers:" >> $f
+echo "                        addresses: [$g]" >> $f
+
+netplan apply
+
+echo ""
 echo "[+] Selesai."
 echo ""
